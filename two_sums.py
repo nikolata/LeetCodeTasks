5bf8e7e5d8aecfd8470e1e50f62a9c622a34e1c6
nikolata <npmetodiev@gmail.com>
2020-04-10 20:12:32 +0300

    two_sums

---
+++ two_sums.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
+
+
+sol = Solution()
+print(sol.twoSum([2, 7, 11, 15], 9))
